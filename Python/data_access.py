b41498c504081006fe8afede1b3e02cd65b21cef
nickarnold97 <nicka97@bu.edu>
2017-10-29 02:59:51 -0400

    Correctly integrated Twillo texting with the user information stored in dynamoDB.

--- Python/data_access.py
+++ Python/data_access.py
@@ -1,6 +1,6 @@
 import boto3
 
-def create_user(phonenumber,name,coords):
+def create_user(phonenumber,name,coords=None):
 	dynamodb = boto3.resource('dynamodb')
 	table = dynamodb.Table('UserData')
 	table.put_item(
@@ -25,19 +25,3 @@ def retreive_user(phonenumber):
 		return item
 	except KeyError as e:
 		return None
-
-
-#create_user('+16508633802','Nick',{'42.351083','-71.110179'})
-pablo = '+16175155778'
-nick = '+16508633802'
-pabloDB = retreive_user(pablo)
-nickDB = retreive_user(nick)
-
-
-if(pabloDB is not None):
-	coords = list(pabloDB['Coordinates'])
-	lat = coords[0]
-	long = coords[1]
-	print('Pablo is at '+str(lat)+", "+str(long))
-else:
-	print('Couldnt find that bitch..... will add')
--- Python/send_sms.py
+++ Python/send_sms.py
@@ -1,3 +1,4 @@
+from Python import data_access
 from flask import Flask, request, session
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
@@ -9,7 +10,8 @@ SECRET_KEY = 'a secret key'  # change this to something appropriate
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# Configure Twilio account
+# Configure Twilio account with public key tied to Pablo Ferreyra's debit card
+
 account_sid = "AC07e0d1e1abc8f066bf7bc4b993cde73c"
 auth_token = "37aa55f8cbae672f3e10e1d080321830"
 
@@ -20,11 +22,11 @@ client = Client(account_sid, auth_token)
     from_="+16175051429",
     body="Hello from the script!")"""
 
-
 # https://4d620adc.ngrok.io is the url currently hosting the script
 @app.route("/", methods=['GET', 'POST'])
 def hello_user():
 	counter = session.get('counter', 0)  # Get counter value, otherwise initialize to 0
+
 	counter += 1  # Increment counter
 	session['counter'] = counter  # Set counter to incremented value
 
@@ -34,36 +36,27 @@ def hello_user():
 	# For debug
 	# print(counter)
 
-	# Replace with database reference
-	callers = {
-		"+14158675309": "Curious George",
-		"+14158675310": "Boots",
-		"+14158675311": "Virgil",
-		"+16176948759": "Gene",
-		"+16175155778": "Pablo",
-	}
-
-	from_number = request.values.get('From', None)  # number text recieved from
-	msg_body = request.values.get('Body', None)  # message body of text
-
-	message2 = callers[from_number] if from_number in callers else "User"  # name lookup in database
+	sender_num = request.values.get('From', None)  # number text recieved from
+	sender_msg = request.values.get('Body', None)  # message body of text
+	user = data_access.retreive_user(sender_num)  # name lookup in database
 
 	respo = MessagingResponse()
-	if (counter == 1 and (from_number in callers)):  # Returning user first text
-		respo.message("Welcome back {}! How far are you running today?".format(message2))
+	if counter == 1 and user is not None:  # Returning user first text
+		respo.message("Welcome back {}! How far are you running today?".format(user['Name']))
 		return str(respo)
-	elif (counter == 1 and (from_number not in callers)):  # New user first text
+	elif counter == 1 and user is None:  # New user first text
 		respo.message("Welcome to FullCircle! Please send us your name")
 		return str(respo)
-	elif (counter == 2 and (from_number not in callers)):  # New user registration (2nd text)
+	elif counter == 2 and user is None:  # New user registration (2nd text)
 		# save from_number,msg_body to database
-		respo.message("Thanks, you have been successfully registered!")
-		session['counter'] = 0
+		respo.message("Thanks, you have been successfully registered! How far are you going to run today?")
+		data_access.create_user(sender_num, sender_msg)
+		session['counter'] = 1
 		return str(respo)
-	elif (counter == 3):  # Back from route
+	elif counter == 3:  # Back from route
 		respo.message("Welcome back! Did you enjoy your route?")
 		return str(respo)
-	elif (counter == 4):  # Feedback from route
+	elif counter == 4:  # Feedback from route
 		# import tone analyzer
 		# number = toneAnalyze(msg_body)
 		# if number > value, happy response
@@ -77,18 +70,18 @@ def hello_user():
 	distance = 0.0
 
 	# Error handling, checking if the user inputs "miles, kilometers" etc
-	if "miles" in str(msg_body):
+	if "miles" in str(sender_msg):
 		unit = "mi"
-		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-	elif "mi" in str(msg_body):
+		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(sender_msg))
+	elif "mi" in str(sender_msg):
 		unit = "mi"
-		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-	elif "km" in str(msg_body):
+		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(sender_msg))
+	elif "km" in str(sender_msg):
 		unit = "km"
-		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-	elif "kilometers" in str(msg_body):
+		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(sender_msg))
+	elif "kilometers" in str(sender_msg):
 		unit = "km"
-		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
+		arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(sender_msg))
 	else:
 		respo.message("I didn't catch that, please enter how far you want to run in km/mi.")
 		counter -= 1  # Decrement counter to prompt again
