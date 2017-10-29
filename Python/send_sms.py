c06a5862df986ea6de2ab689767f21cb0473293b
gdgrin <genedgrinberg@gmail.com>
2017-10-28 22:53:46 -0400

    Working char bot

--- Python/send_sms.py
+++ Python/send_sms.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request, session
 from twilio.rest import Client
-from flask import request
 from twilio.twiml.messaging_response import MessagingResponse
+from datetime import datetime, timedelta
 
 import re
 
+SECRET_KEY = 'a secret key'
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 account_sid = "AC07e0d1e1abc8f066bf7bc4b993cde73c"
 auth_token  = "37aa55f8cbae672f3e10e1d080321830"
@@ -21,7 +23,19 @@ client = Client(account_sid, auth_token)
 @app.route("/", methods=['GET', 'POST'])
 def hello_user():
     """Respond and greet the caller by name."""
-    # Try adding your own number to this list!
+
+    counter = session.get('counter', 0)
+
+    counter += 1
+
+    session['counter'] = counter
+    #session['counter'] = 0
+    
+    expires=datetime.utcnow() + timedelta(minutes=1)
+
+    print(counter)
+
+
     callers = {
         "+14158675309": "Curious George",
         "+14158675310": "Boots",
@@ -32,37 +46,53 @@ def hello_user():
     
     from_number = request.values.get('From', None)
     msg_body = request.values.get('Body', None)
-    print(from_number)
-    print(msg_body)
 
-    message2 = callers[from_number] if from_number in callers else "Monkey"
+    message2 = callers[from_number] if from_number in callers else "User"
 
     #flag = True
     respo = MessagingResponse()
-    #respo.message("{}, thanks for the message!".format(message2))
-    arr = []
+    if(counter==1):
+        respo.message("{}, welcome back! How far are you running today?".format(message2))
+        return str(respo)
 
+    arr = []
+    unit = ""
+    distance = 0.0
     #Error handling, checking if the user inputs "miles" etc
     #while flag:    
     if "miles" in str(msg_body):
-        respo.message("{}, thanks for the correct inputs!".format(message2))
+        unit = "mi"
         arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-        print(arr)
     elif "mi" in str(msg_body):
-        respo.message("{}, thanks for the correct inputs!".format(message2))
-        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-        print(arr)   
+        unit = "mi"
+        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))  
     elif "km" in str(msg_body):
-        respo.message("{}, thanks for the correct inputs!".format(message2))
-        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-        print(arr)    
+        unit = "km"
+        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))  
     elif "kilometers" in str(msg_body):
-        respo.message("{}, thanks for the correct inputs!".format(message2))
-        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))
-        print(arr)    
+        unit = "km"
+        arr = re.findall(r"[-+]?\d*\.\d+|\d+", str(msg_body))  
     else:
-        respo.message("{}, wrong inputs!".format(message2))
+        respo.message("I didn't catch that, please enter how far you want to run in km/mi.")
+        return str(respo)
 
+
+    if(unit=="km" and len(arr) != 0):
+        #arr[0] is in km
+        distance = float(arr[0])
+    elif(unit=="mi" and len(arr) != 0):
+        #convert arr[0]
+        distance = float(arr[0])*1.60934
+        unit="km"
+    else:
+        respo.message("I didn't catch that, please enter how far you want to run in km/mi.")
+        return str(respo)
+    
+    distance = round(distance,1)
+
+    #respo.message("You want to run {} {}".format(distance, unit))
+    respo.message("Generating a route for you!")
+    session['counter'] = 0
     return str(respo)
 
     '''
